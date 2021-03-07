ccba84a7affa2508668be67640ab059cc7906bf6
Tina G <tina@smartlogic.io>
2021-03-07 17:40:02 -0500

    Home Page: Updates page with more styling
    +Adds a layout using the wrapper comonent
    +Renders a user profile card with hardcoded values for now

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,20 @@
+import { Box, Text } from '@chakra-ui/react';
+
+import Navbar from '../components/Navbar';
+import Wrapper from '../components/Wrapper';
 
 const Index = () => (
-  <div>Hello World</div>
-)
+  <>
+    <Navbar />
+    <Wrapper>
+      <Box as="section" bg={"white"} py="12" boxShadow="lg" borderRadius="lg">
+        <Box maxW={{ base: 'xl', md: '7xl' }} mx="auto" px={{ md: '8' }}>
+          <Text fontSize="lg" fontWeight="bold">Username</Text>
+          <Text fontSize="md" fontWeight="normal">created at</Text>
+        </Box>
+      </Box>
+    </Wrapper>
+  </>
+);
 
 export default Index;
