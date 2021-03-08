c636ece1c1e88149072e3fe51906b0e45c15f5b7
Tina G <tina@smartlogic.io>
2021-03-07 20:45:52 -0500

    Cache Login,Register: Adds updateQuery urql client
    +Adds typesafeUpdateQuery for better typing on the urql
    exchange-graphcache updateQuery function
    +Adds exchanges attribute to the urql client for caching or refetching
    the currentUser after login or register mutations are executed

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,49 @@
 import { ChakraProvider, ColorModeProvider } from '@chakra-ui/react';
-import { Provider, createClient } from 'urql';
+import { cacheExchange, QueryInput, Cache} from '@urql/exchange-graphcache';
+import { Provider, createClient, dedupExchange, fetchExchange } from 'urql';
+import { CurrentUserDocument, CurrentUserQuery, LoginDocument, LoginMutation, RegisterMutation } from '../generated/graphql';
 
 import theme from '../theme';
 
+function typesafeUpdateQuery<Result, Query>(
+  cache: Cache,
+  queryInput: QueryInput,
+  result: any,
+  fn: (r: Result, q: Query) => Query
+) {
+  return cache.updateQuery(queryInput, (data) => fn(result, data as any) as any);
+}
 const client = createClient({
   url: "http://localhost:4000/graphql", fetchOptions: {
-  credentials: "include"
-}});
+  credentials: "include",
+  },
+  exchanges: [dedupExchange, cacheExchange({
+    updates: {
+      Mutation: {
+        login: (result, args, cache, info) => {
+          typesafeUpdateQuery<LoginMutation, CurrentUserQuery>(cache, { query: CurrentUserDocument }, result, (result, query) => {
+            if (result.login.errors) { return query; } else {
+              return {
+              currentUser: result.login.user
+            }
+          }
+
+          })
+        },
+        register: (result, args, cache, info) => {
+          typesafeUpdateQuery<RegisterMutation, CurrentUserQuery>(cache, { query: CurrentUserDocument }, result, (result, query) => {
+            if (result.register.errors) { return query; } else {
+              return {
+              currentUser: result.register.user
+            }
+          }
+
+          })
+        }
+      }
+    }
+  }), fetchExchange]
+});
 
 function MyApp({ Component, pageProps }) {
   return (
