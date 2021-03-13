e4e90114eb19b92f21ae393c68572b5afa2d14ec
Tina G <tina@smartlogic.io>
2021-03-13 17:31:13 -0500

    Post: Adds author id to post card and formats

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,28 +1,46 @@
-import { Box, Text } from "@chakra-ui/react";
-import React from "react";
-import { Post as PostType } from "../generated/graphql";
+import { Box, Text } from '@chakra-ui/react';
+import React from 'react';
+import { Post as PostType } from '../generated/graphql';
 
 interface IPostProps {
-	post: Omit<PostType, "updatedAt">;
+  post: Omit<PostType, 'updatedAt' | '__typename' | 'author'>;
 }
 
 const Post: React.FC<IPostProps> = ({ post }) => {
-	const createdAt = new Date(+post.createdAt);
-	return (
-		<Box as="section" bg={'white'} py="12" my={4} boxShadow="lg" borderRadius="lg">
+  const createdAt = new Date(+post.createdAt);
+  return (
+    <Box
+      as="section"
+      bg={'white'}
+      paddingTop="12"
+      paddingBottom="6"
+      my={4}
+      boxShadow="lg"
+      borderRadius="lg"
+    >
       <Box maxW={{ base: 'xl', md: '7xl' }} mx="auto" px={{ md: '8' }}>
-        <Text fontSize="xs" fontWeight="normal" color="gray.400">
-          {createdAt.toLocaleDateString()}
-        </Text>
+        <Box display="flex" justifyContent="space-between" alignItems="center" marginBottom={4}>
+          <Text fontSize="xs" fontWeight="normal" color="gray.400">
+            {createdAt.toLocaleDateString()}
+          </Text>
+          <Text fontSize="xs" fontWeight="normal" color="gray.400">
+            {post.claps}
+          </Text>
+        </Box>
         <Text fontSize="lg" fontWeight="bold">
           {post.title}
         </Text>
         <Text fontSize="medium" fontWeight="normal">
           {post.description}
         </Text>
+        <Box marginTop={4}>
+        <Text fontSize="medium" fontWeight="normal">
+          Author Component: {post.authorId}
+        </Text>
+        </Box>
       </Box>
     </Box>
   );
-}
+};
 
 export default Post;
